97072cbca99a8220a42ccf9b94cd772445af9fc0
a.fathollahi <a.fathollahi@fwutech.com>
2025-09-24 10:04:47 +0330

    deploy-problem-resolve7

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,11 +2,7 @@
 import text from "../../public/locales/fa/common.json"
 import LoginForm from "./loginForm"
 
-export default function LoginPage({
-  searchParams,
-}: {
-  searchParams?: { callbackUrl?: string }
-}) {
+export default async function LoginPage({ searchParams }: { searchParams?: { callbackUrl?: string } }) {
   const callbackUrl = searchParams?.callbackUrl ?? "/profile"
 
   return <LoginForm callbackUrl={callbackUrl} text={text} />
